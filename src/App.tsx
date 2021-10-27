c8ece08ccb0bd3d11299cdf0026b1d0d18ff67af
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2021-10-27 15:01:49 -0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components';
+import { Suspense } from 'react';
+import React from 'react';
+
+export const LazyWelcome = React.lazy(() => import(/* webpackChunkName: 'welcome' */ './pages/Welcome'));
+export const LazyItems = React.lazy(() => import(/* webpackChunkName: 'items' */ './pages/Items'));
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+
+        <Suspense fallback="Loading...">
+          <Switch>
+            <Route component={LazyItems} path="/:slug" />
+            <Route component={LazyWelcome} path="/" exact={true} />
+          </Switch>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
