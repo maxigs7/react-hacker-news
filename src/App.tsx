01d1aa9707c11db23e42754d5112e11832c0b58b
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2021-10-27 14:53:06 -0300

    Add navbar and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import styles from './navbar.module.css';
+
+export const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.menu}>
+        <NavLink to="/" activeClassName={styles.active} className={styles.link} exact={true}>
+          Welcome
+        </NavLink>
+        <NavLink to="/newstories" activeClassName={styles.active} className={styles.link}>
+          News
+        </NavLink>
+        <NavLink to="/topstories" activeClassName={styles.active} className={styles.link}>
+          Top
+        </NavLink>
+        <NavLink to="/beststories" activeClassName={styles.active} className={styles.link}>
+          Best
+        </NavLink>
+      </div>
+    </div>
+  );
+};
