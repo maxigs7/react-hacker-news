0f498f8f41c82ffd30434e2fd44fc027b5ff581c
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2021-10-27 15:05:50 -0300

    Welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const WelcomePage: React.FC = () => {
-  return <h1>Welcome Page</h1>;
+  return (
+    <>
+      <h1>Welcome Hacker News Lab</h1>
+      <Link to="/newstories" className="Button">
+        Check new items
+      </Link>
+    </>
+  );
 };
 
 export default WelcomePage;
